d87ba8946a2f22cf03697bda3fb0e205e1279316
Vazquez Cesar <cesar.evc@outlook.com>
2020-11-07 17:35:26 -0600

    initial webpack, babel and react config

---
+++ webpack.config.js
@@ -0,0 +1,96 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.[contentHash].js'
+    },
+    optimization: {
+        splitChunks: {
+			chunks: 'all',
+			name: false,
+		},
+		runtimeChunk: true,
+    },
+    devServer: {
+        port: 6700
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)/,
+                exclude: /node_modules/,
+                use: { 
+                    loader: 'babel-loader'
+                },
+                
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: { name: 'img/[name].[ext]', limit: 10000 },
+                    },
+                ],
+            },
+            {
+                test: /\.(eot|ttf|woff|woff2|svg|otf)$/,
+                use: [{ loader: 'file-loader' }],
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader', 
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            hash: true,
+			chunksSortMode: 'none',
+			minify: {
+                removeComments: true,
+				collapseWhitespace: true,
+			},
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+			chunkFilename: 'css/[name].[contenthash:8].chunk.css',
+        }),
+        new MomentLocalesPlugin({
+            localesToKeep: ['es'],
+		})
+    ],
+    resolve:{
+        extensions: ['.js', '.jsx']
+    }
+}
