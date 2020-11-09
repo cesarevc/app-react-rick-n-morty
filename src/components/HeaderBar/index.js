831ad285b388dbb66c2d5b1822fd0d01be321944
Vazquez Cesar <cesar.evc@outlook.com>
2020-11-08 19:17:13 -0600

    logo issue hot fix

--- src/components/HeaderBar/index.js
+++ src/components/HeaderBar/index.js
@@ -8,7 +8,7 @@ function HeaderBar (){
 
     return (
         <section className="App-SearchBar">
-            <img src="../../../public/LOGO.png" alt="logo"  style={{height:120, width: 290, marginBottom: 30}} />
+            <img src="https://occ-0-1722-92.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABVK-867iNzC3GeSiDQJ7jasFpdN4ySy2Of17S2KxaxbOOtsqax_k_ldd_f5TiDeulU3_lyJmIjtBgPVKLnE1cUK-kRk9yZsO4MXA.png?r=47e" alt="logo"  style={{height:120, width: 290, marginBottom: 30}} />
             <InputSearch />
         </section>
     );
